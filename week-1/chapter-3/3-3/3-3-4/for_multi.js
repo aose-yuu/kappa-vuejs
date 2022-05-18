d4ea92ec287634337239aed1b25bdf43d1c1f77e
aose-yuu <aose.developer@gmail.com>
2022-05-19 00:29:29 +0900

    [add]for_multi

---
+++ week-1/chapter-3/3-3/3-3-4/for_multi.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#app',
+  data: {
+    songs: [
+      {
+        title: '赤とんぼ',
+        lyrics: '夕焼け小焼けの赤とんぼ',
+        composer: '山田耕作'
+      },
+      {
+        title: '荒城の月',
+        lyrics: '春高桜の花の宴',
+        composer: '滝廉太郎'
+      },
+      {
+        title: 'どんぐりころころ',
+        lyrics: 'どんぐりころころどんぶりこ',
+        composer: '染田貞'
+      }
+    ]
+  }
+})
