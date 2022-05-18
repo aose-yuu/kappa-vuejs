1c65aa2e7bc7cf74b87fb27d27fd5aabf3e80f2a
aose-yuu <aose.developer@gmail.com>
2022-05-19 00:24:32 +0900

    [add]for2

---
+++ week-1/chapter-3/3-3/3-3-4/for2.js
@@ -0,0 +1,34 @@
+new Vue({
+  el: '#app',
+  data: {
+    books: [
+      {
+        isbn: '123-1-1234-1234-1',
+        title: 'JavaScript逆引きレシピ',
+        price: 2800
+      },
+      {
+        isbn: '123-1-1234-1234-1',
+        title: '楽しいラズパイ電子工作ブック',
+        price: 1900
+      },
+      {
+        isbn: '123-1-1234-1234-1',
+        title: '3ステップでしっかり学ぶPython入門',
+        price: 2480 
+      },
+      {
+        isbn: '123-1-1234-1234-1',
+        title: '独習C# 新版',
+        price: 3600
+      },
+    ]
+  },
+  computed: {
+    expensiveBooks: function () {
+      return this.books.filter(function (b) {
+        return b.price >= 2500;
+      })
+    }
+  }
+});
